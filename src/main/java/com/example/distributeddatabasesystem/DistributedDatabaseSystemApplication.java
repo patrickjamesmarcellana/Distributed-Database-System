b6a8a76e0ece10d3122ee4371c45750518959c3b
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-04-06 11:31:31 +0800

    Change files

---
+++ src/main/java/com/example/distributeddatabasesystem/DistributedDatabaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributeddatabasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedDatabaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedDatabaseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/distributeddatabasesystem/DistributedDatabaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributeddatabasesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedDatabaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
